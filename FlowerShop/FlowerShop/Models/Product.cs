5a4b8c719d122971d4783fe5a7bd15b94198975b
maria <lebedeva.maria@stud.onu.edu.ua>
2026-03-19 23:50:52 +0200

    Initial commit

---
+++ FlowerShop/FlowerShop/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace FlowerShop.Models;
+
+public class Product
+{
+    public int Id { get; init; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public decimal? OldPrice { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public bool IsSpecial { get; set; }
+}
